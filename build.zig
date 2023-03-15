d5c556117173758fc5216c325414860735854abc
praschke <stel@comfy.monster>
2023-03-15 20:01:18 +0000

    update zig

--- build.zig
+++ build.zig
@@ -1,17 +1,16 @@
 const std = @import("std");
-const zbs = std.build;
+const zbs = std.Build;
 const fs = std.fs;
 const mem = std.mem;
 
-pub fn build(b: *zbs.Builder) void {
+pub fn build(b: *zbs) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
     const scanner = ScanProtocolsStep.create(b);
-    const wayland = zbs.Pkg{
-        .name = "wayland",
-        .source = .{ .generated = &scanner.result },
-    };
+    const wayland = b.createModule(.{
+        .source_file = .{ .generated = &scanner.result },
+    });
 
     scanner.generate("wl_compositor", 1);
     scanner.generate("wl_shm", 1);
@@ -19,12 +18,15 @@ pub fn build(b: *zbs.Builder) void {
     scanner.generate("wl_output", 1);
 
     inline for ([_][]const u8{ "globals", "list", "listener", "seats" }) |example| {
-        const exe = b.addExecutable(example, "example/" ++ example ++ ".zig");
-        exe.setTarget(target);
-        exe.setBuildMode(mode);
+        const exe = b.addExecutable(.{
+            .name = example,
+            .root_source_file = .{ .path = "example/" ++ example ++ ".zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
         exe.step.dependOn(&scanner.step);
-        exe.addPackage(wayland);
+        exe.addModule("wayland", wayland);
         scanner.addCSource(exe);
         exe.linkLibC();
         exe.linkSystemLibrary("wayland-client");
@@ -34,22 +36,26 @@ pub fn build(b: *zbs.Builder) void {
 
     const test_step = b.step("test", "Run the tests");
     {
-        const scanner_tests = b.addTest("src/scanner.zig");
-        scanner_tests.setTarget(target);
-        scanner_tests.setBuildMode(mode);
+        const scanner_tests = b.addTest(.{
+            .root_source_file = .{ .path = "src/scanner.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
         scanner_tests.step.dependOn(&scanner.step);
-        scanner_tests.addPackage(wayland);
+        scanner_tests.addModule("wayland", wayland);
 
         test_step.dependOn(&scanner_tests.step);
     }
     {
-        const ref_all = b.addTest("src/ref_all.zig");
-        ref_all.setTarget(target);
-        ref_all.setBuildMode(mode);
+        const ref_all = b.addTest(.{
+            .root_source_file = .{ .path = "src/ref_all.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
 
         ref_all.step.dependOn(&scanner.step);
-        ref_all.addPackage(wayland);
+        ref_all.addModule("wayland", wayland);
         scanner.addCSource(ref_all);
         ref_all.linkLibC();
         ref_all.linkSystemLibrary("wayland-client");
@@ -124,10 +130,10 @@ pub const ScanProtocolsStep = struct {
 
         const wayland_dir = mem.trim(u8, try self.builder.exec(
             &[_][]const u8{ "pkg-config", "--variable=pkgdatadir", "wayland-scanner" },
-        ), &std.ascii.spaces);
+        ), &std.ascii.whitespace);
         const wayland_protocols_dir = mem.trim(u8, try self.builder.exec(
             &[_][]const u8{ "pkg-config", "--variable=pkgdatadir", "wayland-protocols" },
-        ), &std.ascii.spaces);
+        ), &std.ascii.whitespace);
 
         const wayland_xml = try fs.path.join(ally, &[_][]const u8{ wayland_dir, "wayland.xml" });
         try self.protocol_paths.append(wayland_xml);
@@ -137,9 +143,9 @@ pub const ScanProtocolsStep = struct {
             try self.protocol_paths.append(absolute_path);
         }
 
-        const out_path = try fs.path.join(ally, &[_][]const u8{ self.builder.cache_root, "zig-wayland" });
+        const out_path = try self.builder.cache_root.join(ally, &[_][]const u8{"zig-wayland"});
 
-        var root_dir = try fs.cwd().openDir(self.builder.build_root, .{});
+        var root_dir = try fs.cwd().openDir(self.builder.build_root.path.?, .{});
         defer root_dir.close();
         var out_dir = try root_dir.makeOpenPath(out_path, .{});
         defer out_dir.close();
@@ -166,12 +172,7 @@ pub const ScanProtocolsStep = struct {
         const basename = fs.path.basename(xml_in_path);
         const basename_no_ext = basename[0..(basename.len - 4)];
         const code_filename = std.fmt.allocPrint(ally, "{s}-protocol.c", .{basename_no_ext}) catch oom();
-        return fs.path.join(ally, &[_][]const u8{
-            self.builder.build_root,
-            self.builder.cache_root,
-            "zig-wayland",
-            code_filename,
-        }) catch oom();
+        return self.builder.cache_root.join(ally, &[_][]const u8{ "zig-wayland", code_filename }) catch oom();
     }
 };
 
